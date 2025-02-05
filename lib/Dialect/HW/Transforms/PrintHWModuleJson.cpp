3bf40cdcbee83c6cbb48b16c85e300f32e2ecbe1
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2025-02-05 00:22:09 +0000

    basic traversal

--- lib/Dialect/HW/Transforms/PrintHWModuleJson.cpp
+++ lib/Dialect/HW/Transforms/PrintHWModuleJson.cpp
@@ -12,6 +12,7 @@
 #include "circt/Dialect/HW/HWOps.h"
 #include "circt/Dialect/HW/HWPasses.h"
 #include "mlir/Pass/Pass.h"
+#include "llvm/Support/JSON.h"
 #include "llvm/Support/raw_ostream.h"
 
 namespace circt {
@@ -30,8 +31,80 @@ struct PrintHWModuleJsonPass
   PrintHWModuleJsonPass(raw_ostream &os) : os(os) {}
   void runOnOperation() override {
     getOperation().walk([&](hw::HWModuleOp module) {
-      os << "hello world\n";
+      llvm::json::Object json = llvm::json::Object();
+
+      json["label"] = module.getName().data();
+
+      // Populate the module attributes
+      llvm::json::Object attributes = llvm::json::Object();
+      if (!module->getAttrs().empty()) {
+        for (NamedAttribute attr : module->getAttrs()) {
+          std::string value;
+          llvm::raw_string_ostream sstream(value);
+          attr.getValue().print(sstream);
+          attributes[attr.getName().str()] = sstream.str();
+        }
+      }
+
+      json["attributes"] = std::move(attributes);
+
+      // Recurse into each of the regions attached to the operation.
+      llvm::json::Array regions;
+      for (Region &region : module->getRegions())
+        regions.push_back(parseRegion(region));
+      json["subgraphs"] = std::move(regions);
+
+      // Output json
+      os << "json: " << llvm::json::Value(std::move(json)) << "\n";
     });
+  }
+
+  llvm::json::Object parseRegion(Region &region) {
+    // A region does not hold anything by itself other than a list of blocks.
+    llvm::json::Object json = llvm::json::Object();
+    json["label"] = "region" + std::to_string(region.getRegionNumber());
+
+    llvm::json::Array blocks;
+    for (Block &block : region.getBlocks()) {
+      blocks.push_back(parseBlock(block));
+    }
+    json["subgraphs"] = std::move(blocks);
+    return json;
+  }
+
+  llvm::json::Object parseBlock(Block &block) {
+    // Print the block intrinsics properties (basically: argument list)
+    // os << "Block with " << block.getNumArguments() << " arguments, "
+    //    << block.getNumSuccessors()
+    //    << " successors, and "
+    //    // Note, this `.size()` is traversing a linked-list and is O(n).
+    //    << block.getOperations().size() << " operations\n";
+
+    llvm::json::Object json = llvm::json::Object();
+
+    // A block main role is to hold a list of Operations: let's recurse into
+    // printing each operation.
+    llvm::json::Array blocks;
+    for (Operation &op : block.getOperations()) {
+      blocks.push_back(parseOp(op));
+    }
+    json["subgraphs"] = std::move(blocks);
+    return json;
+  }
+
+  llvm::json::Object parseOp(Operation &op) {
+    llvm::json::Object json = llvm::json::Object();
+    json["label"] = op.getName().getStringRef().str();
+
+    llvm::json::Array operands;
+    for (Value operand : op.getOperands()) {
+      std::string value;
+      llvm::raw_string_ostream sstream(value);
+      operand.printAsOperand(sstream, mlir::OpPrintingFlags());
+      operands.push_back(llvm::json::Object{{"label", sstream.str()}});
+    }
+    json["subgraphs"] = std::move(operands);
+    return json;
   }
   raw_ostream &os;
 };
