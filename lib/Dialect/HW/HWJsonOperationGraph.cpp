097fecd77bc8d8d6d7cf28fd52488fbc6add926a
Kacper Michalik <km924@cam.ac.uk>
2025-03-11 12:12:52 +0000

    Added comment headers.

--- lib/Dialect/HW/HWJsonOperationGraph.cpp
+++ lib/Dialect/HW/HWJsonOperationGraph.cpp
@@ -1,3 +1,15 @@
+//===- HWJsonOperationGraph.cpp - Model graph JSON generation ---*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the implementation of the Model Explorer JSON generation
+//
+//===----------------------------------------------------------------------===//
+
 #include "circt/Dialect/HW/HWModelExplorer.h"
 #include "circt/Dialect/HW/HWModuleGraph.h"
 #include "circt/Dialect/HW/HWOps.h"
